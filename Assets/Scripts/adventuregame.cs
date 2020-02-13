8e0ebf7083f9594776d5cfca5b4b350daf87ff3b
jperkins01 <jaredperkins2001@gmail.com>
2020-02-13 14:41:48 -0500

    Create a public method

--- Assets/Scripts/adventuregame.cs
+++ Assets/Scripts/adventuregame.cs
@@ -7,11 +7,14 @@ public class adventuregame : MonoBehaviour
 {
 
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
 
+    State state;
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = startingState;
+        textComponent.text = state.GetStateStory();  
     }
 
     // Update is called once per frame
