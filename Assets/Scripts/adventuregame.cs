07d4b01cb930a95719ddf03a3016afba0641ce82
jperkins01 <jaredperkins2001@gmail.com>
2020-02-13 15:32:23 -0500

    Manage Next States

--- Assets/Scripts/adventuregame.cs
+++ Assets/Scripts/adventuregame.cs
@@ -9,7 +9,7 @@ public class adventuregame : MonoBehaviour
     [SerializeField] Text textComponent;
     [SerializeField] State startingState;
 
-    string[] daysOfTheWeek = { "mon", "tues", "Wed", "Thurs", "Friday" };
+    
 
     State state;
     // Start is called before the first frame update
@@ -17,12 +17,32 @@ public class adventuregame : MonoBehaviour
     {
         state = startingState;
         textComponent.text = state.GetStateStory();
-        Debug.Log(daysOfTheWeek[2]);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
     }
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+
+
+
 }
